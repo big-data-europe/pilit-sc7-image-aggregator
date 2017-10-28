b1dd79b720c825fdd3f2b45763c50fea09648811
indiana <gioargyr@gmail.com>
2017-10-28 19:30:29 +0300

    Returning polygon of quick-look fixed.

--- src/eu/bde/sc7pilot/imageaggregator/Workflow.java
+++ src/eu/bde/sc7pilot/imageaggregator/Workflow.java
@@ -153,9 +153,9 @@ public class Workflow {
 			JSONObject img1JSON = new JSONObject();
 			JSONObject img2JSON = new JSONObject();
 			img1JSON.put("url", qlook1File.getAbsolutePath());
-			img1JSON.put("extent", images.get(0).getFootPrint());	// 'H img1JSON.put("extent", images.get(0).getFootPrint());
+			img1JSON.put("extent", images.get(3).getFootPrint());	// 'H img1JSON.put("extent", images.get(0).getFootPrint());
 			img2JSON.put("url", qlook2File.getAbsolutePath());
-			img2JSON.put("extent", images.get(1).getFootPrint());
+			img2JSON.put("extent", images.get(4).getFootPrint());
 			imagesList.add(img1JSON);
 			imagesList.add(img2JSON);
 			responseJSON.put("images", imagesList);
